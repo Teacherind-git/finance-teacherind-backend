f88d0abdf89c8e73775e885085cb09f11fe27841
Muhammed Thanseem <muhammed.thanseem@trivand.com>
2026-03-24 11:58:21 +0530

    vulnerability reduced to 0 and download pdf issue fixed

--- controllers/primary/finance/staffSalaryController.js
+++ controllers/primary/finance/staffSalaryController.js
@@ -356,7 +356,10 @@ exports.downloadReceipt = async (req, res) => {
 
     const html = salarySlipTemplate(data);
 
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
 
